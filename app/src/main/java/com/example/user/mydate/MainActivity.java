1fbb8245e8c364d9d91202a6105b52a5ed1fbff2
LPZ <1503463568@qq.com>
2020-04-09 13:29:15 +0800

    初次提交

---
+++ app/src/main/java/com/example/user/mydate/MainActivity.java
@@ -0,0 +1,55 @@
+package com.example.user.mydate;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Gravity;
+import android.view.View;
+import android.view.Window;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.lpz0715.wheelview.ChangeDatePopwindow;
+
+public class MainActivity extends Activity {
+
+    private TextView selectDate;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.activity_main);
+        selectDate= (TextView) findViewById(R.id.selectDate);
+    }
+
+    public void selectDate(View v){
+        if (v.getId()==R.id.selectDate){
+            selectDate();
+        }
+
+    }
+
+
+    private String[] selectDate() {
+        final String[] str = new String[10];
+        ChangeDatePopwindow mChangeBirthDialog = new ChangeDatePopwindow(
+                this);
+        mChangeBirthDialog.setDate("2016", "1", "1");
+        mChangeBirthDialog.showAtLocation(selectDate, Gravity.BOTTOM, 0, 0);
+        mChangeBirthDialog.setBirthdayListener(new ChangeDatePopwindow.OnBirthListener() {
+
+            @Override
+            public void onClick(String year, String month, String day) {
+                // TODO Auto-generated method stub
+                Toast.makeText(MainActivity.this,year + "-" + month + "-" + day,Toast.LENGTH_LONG).show();
+                StringBuilder sb = new StringBuilder();
+                sb.append(year.substring(0, year.length() - 1)).append("-").append(month.substring(0, day.length() - 1)).append("-").append(day);
+                str[0] = year + "-" + month + "-" + day;
+                str[1] = sb.toString();
+                selectDate.setText(year + "-" + month + "-" + day);
+
+            }
+        });
+        return str;
+    }
+
+}
